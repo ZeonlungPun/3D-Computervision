bd5091d77c44cff0b32e4235ceea124bb29ec8c9
ZeonLungPun <122013127+ZeonlungPun@users.noreply.github.com>
2026-01-31 16:14:48 +0800

    Add files via upload

--- 2-view-reconstruction/include/processor.h
+++ 2-view-reconstruction/include/processor.h
@@ -8,7 +8,7 @@ std::vector<Eigen::Vector3d>cart2hom(std::vector<cv::Point2f>pt_group);
 
 Eigen::Vector2d ReconstructionError(Eigen::Vector4d P_3d,Eigen::Matrix<double, 3, 4> project_matrix,Eigen::Vector2d p_2d_obs);
 
-float TotalReconstructionError(std::vector<Eigen::Vector4d>& tripoints3d,
+double TotalReconstructionError(std::vector<Eigen::Vector4d>& tripoints3d,
     std::vector<Eigen::Matrix<double, 3, 4>>& project_matrix_list,
     std::vector<Eigen::Vector2d>& p_2d_obs_list);
 
